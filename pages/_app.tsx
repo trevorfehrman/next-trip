e35123d0e7f23624294933ee4a8e3ca0994c983a
Trevor Fehrman <trevorfehrman@mynd-c02zh0tglvdv-trevorfehrman.home>
2021-03-01 02:29:42 -0800

    hacky fix for animation style unload on all but the first animation, investigation continues, but this is better than nothing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,8 @@ import { IRouteDirection } from 'interfaces';
 
 import { Header } from 'components/header';
 
+import Router from 'next/router';
+
 export const RouteContext = React.createContext<{
   routeDirection: IRouteDirection;
   setRouteDirection: React.Dispatch<React.SetStateAction<IRouteDirection>>;
@@ -16,6 +18,23 @@ export const RouteContext = React.createContext<{
 RouteContext.displayName = 'RouteContext';
 
 export default function App({ Component, pageProps, router }: AppProps) {
+  const routeChange = () => {
+    // Temporary fix to avoid flash of unstyled content
+    // during route transitions. Keep an eye on this
+    // issue and remove this code when resolved:
+    // https://github.com/vercel/next.js/issues/17464
+
+    const tempFix = () => {
+      const allStyleElems = document.querySelectorAll('style[media="x"]');
+      allStyleElems.forEach(elem => {
+        elem.removeAttribute('media');
+      });
+    };
+    tempFix();
+  };
+
+  Router.events.on('routeChangeComplete', routeChange);
+  Router.events.on('routeChangeStart', routeChange);
   const [routeDirection, setRouteDirection] = React.useState<IRouteDirection>({
     route: '',
     direction: '',
